0d2e0c6adf5f91165570636c0b59cd670a03774e
stefanycoimbra <stefany.ccoimbra@gmail.com>
2022-07-19 02:25:49 -0300

    ADDRDecoding considerando 32 bits e testbench com endereços fora e dentro do intervalo de seleção

--- ADDRDecoding/ADDRDecoding_TB.v
+++ ADDRDecoding/ADDRDecoding_TB.v
@@ -2,23 +2,30 @@
 
 module ADDRDecoding_TB();
 	
-	reg [15:0] Address;
-	wire Saida;
+	reg [31:0] Address;
+	wire CS;
 	
 	ADDRDecoding DUT(
 		.Address(Address),
-		.Saida(Saida)
+		.CS(CS)
 	);
 	
 	integer i;
 	
 	initial begin
-		for(i = 0; i <= 2**15; i = i + 1)
-			#20 Address = i;
+		Address = 32'h0000;
+	
+		// endereço anterior ao intervalo
+		#50 Address = 32'h04FF;
+		// endereços compreendidos dentro do intervalo em que CS é selecionado
+		for(i = 1279; i <= 2048; i = i + 256)
+			#50 Address = i;
+		// endereço após o intervalo
+		#50 Address = 32'h0900;
 	end
 	
 	initial begin
-		#655370 $stop;
+		#600 $stop;
 	end
 	
 endmodule
